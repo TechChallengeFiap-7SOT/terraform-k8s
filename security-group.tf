75e1960d85f76565647c5bba6d428497a4def944
Murakami <maurimurakami97@gmail.com>
2024-09-29 00:42:19 -0300

    test sg

--- security-group.tf
+++ security-group.tf
@@ -6,8 +6,8 @@ resource "aws_security_group" "sg" {
   # Inbound
   ingress {
     description = "HTTP"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 0
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
